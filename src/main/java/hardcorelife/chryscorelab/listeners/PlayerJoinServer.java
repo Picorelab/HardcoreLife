f44b93c2f32716b784f7bd3d1f11473685d682cf
drewburr <drewburr9@gmail.com>
2021-08-29 15:43:41 -0500

    Add support for publishing global lives message Also updated wording and comment placement

--- src/main/java/hardcorelife/chryscorelab/listeners/PlayerJoinServer.java
+++ src/main/java/hardcorelife/chryscorelab/listeners/PlayerJoinServer.java
@@ -14,21 +14,22 @@ public class PlayerJoinServer implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) throws IOException {
-        // Ensures joining players have lives configured
 
         Player player = event.getPlayer();
 
+        // Ensure joining players have lives configured
         if (!player.hasPlayedBefore()) {
             PlayerLife.initPlayer(player);
         }
 
-        // TODO - Show messages about the number of lives remaining server/player, sending to the players (even more, show a history of past events)
-        if(Touchy.globalLivesEnabled){
-            player.sendMessage(" Welcome back aboard " + player.getName() + "! The server have " /* +  getServerLives() */ + " remaining");
-        }else{
-            player.sendMessage("Welcome back aboard " + player.getName() + "! You have " + PlayerLife.getLives(player) + " remaining");
+        // TODO - Show messages about the number of lives remaining server/player,
+        // sending to the players (even more, show a history of past events)
+        if (Touchy.globalLivesEnabled) {
+            player.sendMessage(" Welcome back aboard " + player.getName() + "! The server has "
+                    + PlayerLife.getLives(player) + " live(s) remaining");
+        } else {
+            player.sendMessage("Welcome back aboard " + player.getName() + "! You have " + PlayerLife.getLives(player)
+                    + " live(s) remaining");
         }
-
-
     }
 }
